32431a8bbab802511a85cda7d437c2c84e336f1f
David Gomez pilipenko <dgomez-p@cbr11s4.42barcelona.com>
2022-02-10 16:33:19 +0100

    New function added

--- libft/TEMP/ft_substr/main.c
+++ libft/TEMP/ft_substr/main.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
-#include <strdio.h>
+#include <stdio.h>
 
 char *ft_substr(char const *s,unsigned int start,size_t len);
 
 int main(void)
 {
+	char *str;
+	char *str_new;
 
+	str="1234567890";
+	str_new=ft_substr(str,7,5);
+	printf("The new string is: %s\n",str_new);
 	return (0);
 }
